3de3746f6157f421cc01d2562c94ccaa50e40327
Kevin Anderson <kevinanderso@gmail.com>
2017-02-07 23:22:54 -0800

    push to github

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # MusicXML
+
+A simple parser for MusicXML files, designed to be used with the MXMLSynth library.  Very much still a work in progress.
+
+Example usage:
+
+    using MusicXML
+    using MXMLSynth
+    using PortAudio
+    using SampledSignals
+
+    cd(joinpath(homedir(), "MusicXML", "mxml_tests"))
+
+    @time begin
+      song = MusicXML.parseMXMLFile("fur_elise.xml")
+      sb = MXMLSynth.synthesizeTrack(song)
+    end
+
+    sbs = SampleBufSource(sb)
+    out = PortAudioStream("default")
+    write(out, sbs)
+    #MXMLSynth.saveTrack(joinpath(homedir(), "furry-lease2.ogg"), sb)
--- src/MusicXML.jl
+++ src/MusicXML.jl
@@ -1,5 +1,78 @@
 module MusicXML
 
-# package code goes here
+using LightXML
 
-end # module
+@enum STEP C=1 D=3 E=5 F=6 G=8 A=10 B=12
+@enum TYPE whole=1 half=2 quarter=3 eighth=4 sixteenth=5 thirtysecond=6
+
+type Pitch
+  step::STEP
+  octave::Int
+  alter::Int
+end
+
+type Note
+  pitch::Pitch
+  duration::Int
+  voice::Int
+  notetype::TYPE
+  isrest::Bool
+end
+
+type Measure
+  notes::Array{Note, 1}
+end
+
+type Part
+  measures::Array{Measure, 1}
+end
+
+type MXMLTree
+  parts::Array{Part, 1}
+end
+
+function parseMXMLFile(fname::String)
+  xmlfile = parse_file(fname)
+  return parseMXMLTree(root(xmlfile))
+end
+
+function parseMXMLTree(root::XMLElement)
+  parts = Part[]
+  for c1 in child_elements(root)
+    if LightXML.name(c1) == "part"
+      measures = Measure[]
+      for c2 in child_elements(c1)
+        if LightXML.name(c2) == "measure"
+          notes = Note[]
+          for c3 in child_elements(c2)
+            if LightXML.name(c3) == "note"
+              c4 = find_element(c3, "pitch")
+              step = A
+              octave = 0
+              alter = 0
+              isrest = true
+              if typeof(c4) != Void
+                step = eval(parse(content(find_element(c4, "step"))))
+                octave = parse(content(find_element(c4, "octave")))
+                alternode = find_element(c4, "alter")
+                alter = typeof(alternode) == Void ? 0 : parse(content(alternode))
+                isrest = false
+              end
+              p = Pitch(step, octave, alter)
+              duration = parse(content(find_element(c3, "duration")))
+              voice = parse(content(find_element(c3, "voice")))
+              notetype = quarter #eval(parse(content(find_element(c3, "type"))))
+
+              push!(notes, Note(p, duration, voice, notetype, isrest))
+            end
+          end
+          push!(measures, Measure(notes))
+        end
+      end
+      push!(parts, Part(measures))
+    end
+  end
+  return MXMLTree(parts)
+end
+
+end
